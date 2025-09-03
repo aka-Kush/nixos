42c96c05dd77d99937e62cc01fffc014e45b4def
Shivansh Kush <62604121+aka-Kush@users.noreply.github.com>
2025-09-04 00:42:24 +0530

    Update hyprland.nix

--- configs/hyprland.nix
+++ configs/hyprland.nix
@@ -167,9 +167,9 @@
                 "$mainMod, down, movefocus, d"
 
                 "$mainMod SHIFT, left, movewindow, l"
-                "$mainMod SHIFT, right, movewindow, d"
-                "$mainMod SHIFT, k, movewindow, u"
-                "$mainMod SHIFT, l, movewindow, r"
+                "$mainMod SHIFT, right, movewindow, r"
+                "$mainMod SHIFT, up, movewindow, u"
+                "$mainMod SHIFT, down, movewindow, d"
 
 
                 # Switch workspaces with mainMod + [0-9]
